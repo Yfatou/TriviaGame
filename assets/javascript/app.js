36bd1f91b9a53cca4eb84af4036498cef114dc9b
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2018-11-02 23:18:18 -0500

    with display result

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,111 +4,157 @@ var setTimeOutID;
 var correctAnswer = 0;
 var uncorrectAnswer = 0;
 var unAnswerred = 0;
-var count = 30;
+var count = 10;
 var current = 0;
+var right = false;
+var answerred = false;
 
 $(document).ready(function(){
 
 var questionArray = [
     {question : "When was William Shakespeare born?",
-    answers :["23rd April 1564","23rd May 1654","15th June 1632", "4th May 1564"],
-    correctAnswer  : "23rd April 1564",
+    answers :["April 26th 1564","May 23rd 1654","June 15th 1632", "May 4th 1564"],
+    rightAnswer  : "April 26th 1564",
     image : ""}, 
     
     {question: "Who was Henry VIII's first wife?",
     answers: ["Cleopatra", "Catherine of Aragon", "Catherine Howard", "Jeanne Seymour"],
-    correctAnswer : "Catherine of Aragon",
+    rightAnswer : "Catherine of Aragon",
     image : ""},
 
     {question: "When did Margaret Thatcher become Prime Minister?",
     answers: ["1982", "1979", "1980", "1975"],
-    correctAnswer: "1979",
+    rightAnswer: "1979",
     image: ""},
 
     {question: "When did the Cold War ended?",
     answers: ["1978", "1985", "1989", "1998"],
-    correctAnswer: "1989",
+    rightAnswer: "1989",
     image: ""},
 
     {question: "When was the euro introduced as legal currency on the world market?",
     answers: ["January, 1st 1999", "January, 1st 2001", "December, 31st 1999", "December, 31st 2001"],
-    correctAnswer: "January, 1st 1999",
+    rightAnswer: "January, 1st 1999",
     image: ""},
 
     {question: "Which British coin was also known as a bob?",
     answers: ["Crown", "Florin", "Shiling", "Penny"],
-    correctAnswer: "Shiling",
+    rightAnswer: "Shiling",
     image: ""}]
 
-//console.log(questionArray[0].question, questionArray[0].answers);
+
+function reset(){
+    $("#questions-zone").empty();
+    $("#answer-zone").empty();
+    right = false;
+    answerred = false;
+};
 
 //This function will get the question in the array and display it in the right div
 //then it take the choice of answers for that question and display them in the div
 function getQuestion() {
+    //$("#questions-zone").empty();
+    //$("#answer-zone").empty();
+    reset();
+
+    if (questionArray[current]){//we are not at the end of the questions array
     $("#timer").html("Time Remaining: " + count + " secs");
     $("#questions-zone").html(questionArray[current].question);
     var answersArr = questionArray[current].answers;
     console.log(answersArr);
-    var buttonsArr = [];
 
-    //For loop that goes trough the answer array and display the different choices
+    //For loop that goes trough the answer array and display the different choices of answer
     $.each(answersArr, function(index){ 
-        //$("#answer-zone").append("<button>"+ answersArr[index] + "</button>");
-        //index++;
-        var a = $("<button>");
-          a.addClass("answer");
-          a.attr("data-name", answersArr[index]);
-          a.text(answersArr[index]);
-          $("#answer-zone").append(a);
-          buttonsArr.push(answersArr[index]);
-          console.log(buttonsArr[index]);
+        //The different choices of answer are displayed in buttons
+        $("#answer-zone").append("<button>"+ answersArr[index] + "</button>");
           index++;
     })
+  
     window.triviaCounter = setInterval(timer, 1000);
+
+    }else { //No more question in the array, game over
+        $("#questions-zone").html("All done, heres how you did!");
+        $("#answer-zone").html("<br> Corrects answers: " + correctAnswer);
+        $("#answer-zone").append("<br> Incorrects answers: " + uncorrectAnswer);
+        $("#answer-zone").append("<br> Unanswered: " + (questionArray.length - (correctAnswer + uncorrectAnswer)));
+        $("#start").text("Start Over?").appendTo("#answer-zone").show();
+    }
+    
 };
 
+//The time function will decrement the time and get to the next question if the time is over
 function timer() {
     count--;
+    //If the time remaining is 0 seconds
     if (count <= 0) {
-        setTimeout(function() {
-            nextQuestion();
+        setTimeout(function() {//TimeOut
+            nextQuestion();    //and we display the other question
         });
 
-    } else {
+    } else {//otherwise it displays the time remaining
         $("#timer").html("Time Remaining: " + count + " secs");
     }
 };
 
+//Function to display the next question
 function nextQuestion() {
-    current++;
-    $("#answer-zone").empty();
+    console.log("right: " + right);
+    console.log("answerred: " + answerred);
+    current++;      //Increment the value of the curret question
+    $("#questions-zone").empty();//Delete the content of the question zone  
+    $("#answer-zone").empty();//Clear the content of the answer zone to avoid having multiple buttons
     clearInterval(window.triviaCounter);
-    count = 30;
+    displayResult();
+    count = 10; //Set the counter to 10 seconds
     $("#timer").html("");
     setTimeout(function() {
-        getQuestion();
-    }, 1000)
+        getQuestion(); //get the next question
+    }, 2000)
 };
 
+//This function, tell the player how he/she did at the previous question
+function displayResult() {
+    if (right == true){
+        $("#timer").html("Time Remaining: " + count + " secs")
+        $("#questions-zone").html("Well done - Congrats!!");
+    }
+    else { if (answerred){
+        $("#timer").html("Time Remaining: " + count + " secs")
+        $("#questions-zone").html("Uh oh Nice Try...");
+        $("#answer-zone").html("The Correct Answer was: " + questionArray[current-1].rightAnswer);
+        } else {
+            $("#timer").html("Time Remaining: " + count + " secs")
+            $("#questions-zone").html("Out of time...");
+            $("#answer-zone").html("The Correct Answer was: " + questionArray[current-1].rightAnswer);
+            }
+    }
+   
+    //$("#answer-zone").append()
 
+    //$('.correct').html('Correct answers: ' + _t.answers.correct);
+    //$('.incorrect').html('Incorrect answers: ' + _t.answers.incorrect);
+};
+
+//The game begin when the user click on the start button
 $("#start").click(function() {
-    $(this).hide();
+    $(this).hide();//hide the start button
     getQuestion();
 });
 
-//$("#answer-zone").on("click", function() {
-$("button").click(function(){  
-    console.log("in button click function") ;
-    var userPick = $(this).val();
-    console.log(userPick);
-    //$(this).data("id"),
-        index = questionArray[current].correctAnswer,
-        correct = questionArray[current].answers[index];
+//When the user click on an answer
+$("#answer-zone").on("click", "button", function() {
+    answerred = true;
+    var userAnswer = $(this).text(); //value of the button clicked
+    rightA = questionArray[current].rightAnswer; //the right expected answer for this question
 
-    if (userPick !== index) {
+    if (userAnswer == rightA) { //if the answer is right
+        right = true;
+        correctAnswer++;
+        console.log("correctAnswer: " + correctAnswer);
+    } else { //wrong answer
+        right = false;
         uncorrectAnswer++;
-    } else {
-       correctAnswer++;
+        console.log("UncorrectAnswer: " + uncorrectAnswer);
     }
     nextQuestion();
 });
