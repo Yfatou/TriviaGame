7e4aa9c3de26bc841507f0c044b6d3375626f29a
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2018-11-02 23:59:47 -0500

    With images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,44 @@
 //Javascript file
-var setIntervalID;
-var setTimeOutID;
-var correctAnswer = 0;
-var uncorrectAnswer = 0;
-var unAnswerred = 0;
-var count = 10;
-var current = 0;
-var right = false;
-var answerred = false;
+var correctAnswer = 0; //Counter for teh correct answers
+var uncorrectAnswer = 0; //Counter for the incorrect answers
+var unAnswerred = 0;//counter for the unanswerred questions
+var count = 10;//the number of seconds to answer a question
+var current = 0; //the id of the current question
+var right = false;//to store if the player answered right or no
+var answerred = false;//to store if the player answered the question or not
 
 $(document).ready(function(){
 
-var questionArray = [
+var questionArray = [ //Array of questions
     {question : "When was William Shakespeare born?",
     answers :["April 26th 1564","May 23rd 1654","June 15th 1632", "May 4th 1564"],
     rightAnswer  : "April 26th 1564",
-    image : ""}, 
+    image : "/TriviaGame/assets/images/shakespeare.jpeg"}, 
     
     {question: "Who was Henry VIII's first wife?",
     answers: ["Cleopatra", "Catherine of Aragon", "Catherine Howard", "Jeanne Seymour"],
     rightAnswer : "Catherine of Aragon",
-    image : ""},
+    image : "/TriviaGame/assets/images/CatherineAragon.jpeg"},
 
     {question: "When did Margaret Thatcher become Prime Minister?",
     answers: ["1982", "1979", "1980", "1975"],
     rightAnswer: "1979",
-    image: ""},
+    image: "/TriviaGame/assets/images/MargaretThatcher.jpeg"},
 
     {question: "When did the Cold War ended?",
     answers: ["1978", "1985", "1989", "1998"],
     rightAnswer: "1989",
-    image: ""},
+    image: "/TriviaGame/assets/images/coldWar.jpeg"},
 
     {question: "When was the euro introduced as legal currency on the world market?",
     answers: ["January, 1st 1999", "January, 1st 2001", "December, 31st 1999", "December, 31st 2001"],
     rightAnswer: "January, 1st 1999",
-    image: ""},
+    image: "/TriviaGame/assets/images/euro.png"},
 
     {question: "Which British coin was also known as a bob?",
     answers: ["Crown", "Florin", "Shiling", "Penny"],
     rightAnswer: "Shiling",
-    image: ""}]
+    image: "/TriviaGame/assets/images/shiling.jpeg"}]
 
 
 function reset(){
@@ -53,8 +51,6 @@ function reset(){
 //This function will get the question in the array and display it in the right div
 //then it take the choice of answers for that question and display them in the div
 function getQuestion() {
-    //$("#questions-zone").empty();
-    //$("#answer-zone").empty();
     reset();
 
     if (questionArray[current]){//we are not at the end of the questions array
@@ -117,15 +113,18 @@ function displayResult() {
     if (right == true){
         $("#timer").html("Time Remaining: " + count + " secs")
         $("#questions-zone").html("Well done - Congrats!!");
+        $("#answer-zone").html("<img src=" + questionArray[current-1].image + ">");
     }
     else { if (answerred){
         $("#timer").html("Time Remaining: " + count + " secs")
-        $("#questions-zone").html("Uh oh Nice Try...");
+        $("#questions-zone").html("Uh oh... Nice Try.");
         $("#answer-zone").html("The Correct Answer was: " + questionArray[current-1].rightAnswer);
+        $("#answer-zone").html("<img src=" + questionArray[current-1].image + ">");
         } else {
             $("#timer").html("Time Remaining: " + count + " secs")
             $("#questions-zone").html("Out of time...");
             $("#answer-zone").html("The Correct Answer was: " + questionArray[current-1].rightAnswer);
+            $("#answer-zone").html("<img src=" + questionArray[current-1].image + ">");
             }
     }
    
